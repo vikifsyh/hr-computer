2941e1ddb3f1a422baae0f7fe826a487831c6af0
Viki Flendiansyah <vfsyah@gmail.com>
2025-05-21 21:18:12 +0700

    refactor: dashboard

--- src/app/(admin)/admin/dashboard/page.tsx
+++ src/app/(admin)/admin/dashboard/page.tsx
@@ -33,34 +33,6 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchCarts = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch("/api/cart"); // URL API
-        if (!response.ok) {
-          throw new Error("Failed to fetch carts");
-        }
-        const data = await response.json();
-        setCarts(data.carts || []); // Pastikan carts diisi dengan array dari API
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCarts();
-  }, []);
-
-  // Menghitung total jumlah pesanan
-  const totalOrders = carts.reduce((total, cart) => {
-    return (
-      total +
-      cart.orderItems.reduce((subTotal, item) => subTotal + item.quantity, 0)
-    );
-  }, 0);
-
   const fetchUsers = async () => {
     try {
       const response = await fetch("/api/profil?all=true");
@@ -117,7 +89,7 @@ export default function Dashboard() {
       </div>
 
       {/* Statistics Section */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
         <div className="bg-blue-100 p-4 rounded-lg shadow-sm">
           <h2 className="text-lg font-medium text-blue-700">Total Pengguna</h2>
           <p className="text-4xl font-bold text-blue-900 mt-2">
@@ -130,63 +102,8 @@ export default function Dashboard() {
             {products.length}
           </p>
         </div>
-        <div className="bg-yellow-100 p-4 rounded-lg shadow-sm">
-          <h2 className="text-lg font-medium text-yellow-700">Pesanan Baru</h2>
-          <p className="text-4xl font-bold text-yellow-900 mt-2">
-            {totalOrders}
-          </p>
-        </div>
       </div>
-      <div className="space-y-4">
-        {carts.map((cart) => (
-          <div
-            key={cart.id}
-            className="border rounded-lg p-4 bg-gray-50 shadow-sm"
-          >
-            <div className="flex justify-between items-center">
-              <h2 className="text-lg font-semibold">{cart.userName}</h2>
-              <span
-                className={`px-3 py-1 text-sm font-medium rounded ${
-                  cart.status === "COMPLETED"
-                    ? "bg-green-100 text-green-700"
-                    : cart.status === "PENDING"
-                    ? "bg-yellow-100 text-yellow-700"
-                    : cart.status === "CART"
-                    ? "bg-blue-100 text-blue-700"
-                    : "bg-red-100 text-red-700"
-                }`}
-              >
-                {cart.status}
-              </span>
-            </div>
-            <p className="text-sm text-gray-500">
-              Total Item: {cart.totalQuantity}
-            </p>
-            <p className="text-sm text-gray-500">Total Harga: {cart.price}</p>
-            <ul className="mt-2 space-y-2">
-              {cart.orderItems.map((item) => (
-                <li
-                  key={item.id}
-                  className="flex items-center gap-4 border p-2 rounded"
-                >
-                  <img
-                    src={item.productImage}
-                    alt={item.productName}
-                    className="w-12 h-12 object-cover rounded"
-                  />
-                  <div>
-                    <h3 className="text-sm font-medium">{item.productName}</h3>
-                    <p className="text-sm text-gray-500">
-                      Jumlah: {item.quantity} | Harga:{" "}
-                      {formatCurrency(parseFloat(item.price))}
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
+
       <div className="bg-primary-50 p-4 rounded-lg mt-4">
         <h2 className="text-lg font-semibold mb-2">Aktivitas Pengguna Baru</h2>
         {users.length === 0 ? (
